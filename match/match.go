e0e61e4d9ac5d0a3c0c6bcaef7ee7a300178f7fd
meximonster <tiltaki@gmail.com>
2020-09-12 15:11:57 +0300

    add stats struct

--- match/match.go
+++ match/match.go
@@ -1,6 +1,7 @@
 package match
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -8,8 +9,71 @@ import (
 	"github.com/valyala/fastjson"
 )
 
+type stats struct {
+	ParticipantID                   int  `json:"participantId"`
+	Win                             bool `json:"win"`
+	Item0                           int  `json:"item0"`
+	Item1                           int  `json:"item1"`
+	Item2                           int  `json:"item2"`
+	Item3                           int  `json:"item3"`
+	Item4                           int  `json:"item4"`
+	Item5                           int  `json:"item5"`
+	Item6                           int  `json:"item6"`
+	Kills                           int  `json:"kills"`
+	Deaths                          int  `json:"deaths"`
+	Assists                         int  `json:"assists"`
+	LargestKillingSpree             int  `json:"largestKillingSpree"`
+	LargestMultiKill                int  `json:"largestMultiKill"`
+	KillingSprees                   int  `json:"killingSprees"`
+	LongestTimeSpentLiving          int  `json:"longestTimeSpentLiving"`
+	DoubleKills                     int  `json:"doubleKills"`
+	TripleKills                     int  `json:"tripleKills"`
+	QuadraKills                     int  `json:"quadraKills"`
+	PentaKills                      int  `json:"pentaKills"`
+	UnrealKills                     int  `json:"unrealKills"`
+	TotalDamageDealt                int  `json:"totalDamageDealt"`
+	MagicDamageDealt                int  `json:"magicDamageDealt"`
+	PhysicalDamageDealt             int  `json:"physicalDamageDealt"`
+	TrueDamageDealt                 int  `json:"trueDamageDealt"`
+	LargestCriticalStrike           int  `json:"largestCriticalStrike"`
+	TotalDamageDealtToChampions     int  `json:"totalDamageDealtToChampions"`
+	MagicDamageDealtToChampions     int  `json:"magicDamageDealtToChampions"`
+	PhysicalDamageDealtToChampions  int  `json:"physicalDamageDealtToChampions"`
+	TrueDamageDealtToChampions      int  `json:"trueDamageDealtToChampions"`
+	TotalHeal                       int  `json:"totalHeal"`
+	TotalUnitsHealed                int  `json:"totalUnitsHealed"`
+	DamageSelfMitigated             int  `json:"damageSelfMitigated"`
+	DamageDealtToObjectives         int  `json:"damageDealtToObjectives"`
+	DamageDealtToTurrets            int  `json:"damageDealtToTurrets"`
+	VisionScore                     int  `json:"visionScore"`
+	TimeCCingOthers                 int  `json:"timeCCingOthers"`
+	TotalDamageTaken                int  `json:"totalDamageTaken"`
+	MagicalDamageTaken              int  `json:"magicalDamageTaken"`
+	PhysicalDamageTaken             int  `json:"physicalDamageTaken"`
+	TrueDamageTaken                 int  `json:"trueDamageTaken"`
+	GoldEarned                      int  `json:"goldEarned"`
+	GoldSpent                       int  `json:"goldSpent"`
+	TurretKills                     int  `json:"turretKills"`
+	InhibitorKills                  int  `json:"inhibitorKills"`
+	TotalMinionsKilled              int  `json:"totalMinionsKilled"`
+	NeutralMinionsKilled            int  `json:"neutralMinionsKilled"`
+	NeutralMinionsKilledTeamJungle  int  `json:"neutralMinionsKilledTeamJungle"`
+	NeutralMinionsKilledEnemyJungle int  `json:"neutralMinionsKilledEnemyJungle"`
+	TotalTimeCrowdControlDealt      int  `json:"totalTimeCrowdControlDealt"`
+	ChampLevel                      int  `json:"champLevel"`
+	VisionWardsBoughtInGame         int  `json:"visionWardsBoughtInGame"`
+	SightWardsBoughtInGame          int  `json:"sightWardsBoughtInGame"`
+	WardsPlaced                     int  `json:"wardsPlaced"`
+	WardsKilled                     int  `json:"wardsKilled"`
+	FirstBloodKill                  bool `json:"firstBloodKill"`
+	FirstBloodAssist                bool `json:"firstBloodAssist"`
+	FirstTowerKill                  bool `json:"firstTowerKill"`
+	FirstTowerAssist                bool `json:"firstTowerAssist"`
+}
+
 // Info returns player stats from a match given matchId
 func Info(id int64, champion int) {
+	var s stats
 	url := "https://euw1.api.riotgames.com/lol/match/v4/matches/" + fmt.Sprint(id)
 	body := utils.GetReq(url)
 	var p fastjson.Parser
@@ -20,8 +84,12 @@ func Info(id int64, champion int) {
 	vv := v.GetArray("participants")
 	for i := range vv {
 		if vv[i].GetInt("championId") == champion {
-			s := vv[i].Get("stats").MarshalTo(nil)
-			fmt.Println(string(s))
+			l := vv[i].Get("stats").MarshalTo(nil)
+			err := json.Unmarshal(l, &s)
+			if err != nil {
+				log.Fatal("Could not unmarshal json", err)
+			}
+			fmt.Println(s.Kills, s.Deaths, s.Assists)
 		}
 	}
 }
